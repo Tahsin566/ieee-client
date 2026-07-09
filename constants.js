ef0232e230547161380d3f8aae87b2eb51dafeb7
Tahsin566 <nazmulhassantahsin566@gmail.com>
2026-07-09 20:42:41 +0600

    uncommited changes

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
 
 
-export const BASE_URL = 'https://ieee-cs-lu-sbcv1.netlify.app'
+export const BASE_URL = 'https://ieee-cs-lu-sbc-v1.onrender.com/api'
 // export const BASE_URL = 'http://localhost:4000'
